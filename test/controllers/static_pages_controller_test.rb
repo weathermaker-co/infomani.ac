0d36dff1de212fe2c07106355db5bb2b90eb6238
weathermaker-co <dustin@weathermaker.co>
2018-09-09 21:19:57 -0400

    Complete advanced setup extra

--- test/controllers/static_pages_controller_test.rb
+++ test/controllers/static_pages_controller_test.rb
@@ -3,8 +3,8 @@ require 'test_helper'
 class StaticPagesControllerTest < ActionDispatch::IntegrationTest
 
   test "should get root" do
-    get FILL_IN
-    assert_response FILL_IN
+    get static_pages_home_url
+    assert_response :success
   end
 
 
